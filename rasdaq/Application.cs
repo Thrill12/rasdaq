712739e66ec9b14ac2d5cde34416e850be7c567b
AndreiPC <andreipatea@gmail.com>
2026-03-08 23:00:17 +0000

    Add project files.

---
+++ rasdaq/Application.cs
@@ -0,0 +1,25 @@
+﻿using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+internal class Application : GameWindow
+{
+    public Application(int width, int height, string title)
+        : base(
+            GameWindowSettings.Default,
+            new NativeWindowSettings() { ClientSize = (width, height), Title = title }
+        )
+    {
+        Console.WriteLine("Application started");
+    }
+
+    protected override void OnUpdateFrame(FrameEventArgs args)
+    {
+        base.OnUpdateFrame(args);
+
+        if (KeyboardState.IsKeyDown(Keys.Escape))
+        {
+            Close();
+        }
+    }
+}
---
+++ rasdaq/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        using (Application app = new Application(800, 600, "Rasdaq"))
+        {
+            app.Run();
+        }
+    }
+}
