169282d6ec41e972840f823a09349b154a2c622f
AndreiPC <andreipatea@gmail.com>
2026-03-16 00:08:20 +0000

    add pong back

---
+++ samples/pong/Program.cs
@@ -0,0 +1,18 @@
+﻿using rasdaq.Graphics;
+using System.Drawing;
+using Application = rasdaq.Application;
+
+namespace Pong;
+
+internal class Program
+{
+    static void Main()
+    {
+        using Application app = new(800, 600, "Pong");
+
+        Sprite spr = new(2f, 1f, "samples/pong/assets/andrei.png");
+        Application.SetBackgroundColor(Color.CornflowerBlue);
+
+        app.Run();
+    }
+}
