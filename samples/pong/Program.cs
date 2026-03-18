43dbf499fcee57e9177375d876f5666d74fefea2
AndreiPC <andreipatea@gmail.com>
2026-03-18 23:31:36 +0000

    updating assets include

--- samples/pong/Program.cs
+++ samples/pong/Program.cs
@@ -8,10 +8,19 @@ internal class Program
 {
     static void Main()
     {
-        using Application app = new(800, 600, "Pong");
+        try
+        {
+            using Application app = new(800, 600, "Pong");
 
-        Texture tex = new("samples/pong/assets/andrei.png");
-        Sprite spr = new(2f, 1f, tex);
-        Application.SetBackgroundColor(Color.CornflowerBlue);
+            Texture tex = new("assets/andrei.png");
+            Sprite spr = new(2f, 1f, Color.Red);
+            Application.SetBackgroundColor(Color.CornflowerBlue);
+
+            app.Run();
+        }
+        catch (Exception ex)
+        {
+            File.WriteAllText("crash.log", ex.ToString());
+        }
     }
 }
