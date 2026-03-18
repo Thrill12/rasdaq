824d62a40f4a040d2e76ddf6e669f896b63da922
Rinceri <rinceri@protonmail.com>
2026-03-18 20:29:28 +0000

    add input manager first draft

--- src/rasdaq/InputManager/InputManager.cs
+++ src/rasdaq/InputManager/InputManager.cs
@@ -1,6 +1,47 @@
+using OpenTK.Graphics.ES20;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+
 namespace rasdaq.InputManager;
 
 public class InputManager
 {
-    // Example for namespace organization
+    private GameWindow _gameWindow;
+    private Dictionary<char, Action> upCallbacks = [];
+    private Dictionary<char, Action> downCallbacks = [];
+
+    public InputManager(GameWindow gameWindow)
+    {
+        _gameWindow = gameWindow;
+    }
+
+    public void AddKeyUpCallback(char key, Action inputCallback)
+    {
+        upCallbacks.Add(key, inputCallback);
+    }
+
+    public void AddKeyDownCallback(char key, Action inputCallback)
+    {
+        downCallbacks.Add(key, inputCallback);
+    }
+    
+    internal void SetEventListeners()
+    {
+        _gameWindow.KeyUp += (e) =>
+        {
+            if (upCallbacks.ContainsKey((char)e.Key))
+            {
+                upCallbacks[(char)e.Key]();
+            }
+        };
+
+        _gameWindow.KeyDown += (e) =>
+        {
+            if (downCallbacks.ContainsKey((char)e.Key))
+            {
+                downCallbacks[(char)e.Key]();
+            }
+        };
+    }
+
 }
