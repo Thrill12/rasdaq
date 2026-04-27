483ec44329d45475596842c59e8ee46c3c8298e6
Rinceri <rinceri@protonmail.com>
2026-04-28 00:04:00 +0100

    add final transformation functions

--- samples/pong/Program.cs
+++ samples/pong/Program.cs
@@ -1,4 +1,5 @@
-﻿using OpenTK.Windowing.GraphicsLibraryFramework;
+﻿using OpenTK.Mathematics;
+using OpenTK.Windowing.GraphicsLibraryFramework;
 using pong;
 using rasdaq.Core.ECS;
 using rasdaq.Graphics;
@@ -50,11 +51,12 @@ internal class Program
     {
         try
         {
-            using Application app = new(800, 600, "rasdaq");
+            Entity soldier = new();
+            using Application app = new(800, 600, "rasdaq", soldier);
             Texture tex = new("assets/andrei.png");
 
             World world = new();
-            Entity soldier = new();
+            // Entity soldier = new();
             soldier.AddComponent(new Soldier());
 
             Sprite spr = new(1.1f, 1.1f, tex);
@@ -68,24 +70,25 @@ internal class Program
             //         Console.WriteLine("X: " + app.InputManager.GetMousePosition().X);
             //         Console.WriteLine("Y: " + app.InputManager.GetMousePosition().Y);
             //     });
-            app.InputManager.KeyDownCallbacks.Add(Keys.W, () =>
-            {
-                soldier.Transform.Move(0, 0.1f);
-                System.Console.WriteLine("yo just got");
-            });
-            app.InputManager.KeyDownCallbacks.Add(Keys.S, () =>
-            {
-                soldier.Transform.Move(0, -0.1f);
-            });
-            app.InputManager.KeyDownCallbacks.Add(Keys.A, () =>
-            {
-                soldier.Transform.Move(-0.1f, 0);
-            });
-            app.InputManager.KeyDownCallbacks.Add(Keys.D, () =>
-            {
-                soldier.Transform.Move(0.1f, 0);
-            });
+            // app.InputManager.KeyDownCallbacks.Add(Keys.W, () =>
+            // {
+            //     soldier.Transform.Move(new Vector2(0, 10));
+            //     System.Console.WriteLine("yo j1ust got");
+            // });
+            // app.InputManager.KeyDownCallbacks.Add(Keys.S, () =>
+            // {
+            //     soldier.Transform.Move(new Vector2(0, -50000));
+            // });
+            // app.InputManager.KeyDownCallbacks.Add(Keys.A, () =>
+            // {
+            //     soldier.Transform.Move(new Vector2(-50000, 0));
+            // });
+            // app.InputManager.KeyDownCallbacks.Add(Keys.D, () =>
+            // {
+            //     soldier.Transform.Move(new Vector2(50000, 0));
+            // });
 
+            soldier.Transform.MoveDistance(new Vector2(100, 0), 10f);
             app.InputManager.mouseMoveAction = (e) =>
             {
                 if (logMouseDelta)
