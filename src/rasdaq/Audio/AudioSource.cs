749cd27915be6bdb6fe65cdcb77a2f894264fde7
Neel R <51024479+nrbyte@users.noreply.github.com>
2026-05-13 23:25:56 +0100

    Log errors too

--- src/rasdaq/Audio/AudioSource.cs
+++ src/rasdaq/Audio/AudioSource.cs
@@ -1,10 +1,13 @@
 ﻿using OpenTK.Audio.OpenAL;
 
+using rasdaq.Logging;
+
 namespace rasdaq.Audio;
 
 public class AudioSource : IDisposable
 {
     public int Handle;
+
     public AudioSource()
     {
         Handle = AL.GenSource();
@@ -12,6 +15,9 @@ public class AudioSource : IDisposable
         ALError error = AL.GetError();
         if (error != ALError.NoError)
         {
+            string err = $"OpenAL error while trying to create audio source: {error}";
+
+            Log.Error(err);
             throw new Exception("OpenAL error while trying to create audio source: " + error);
         }
     }
