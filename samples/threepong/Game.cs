a41d262399ec944b3fa6a933a7500e314c30c126
Andrei Lodestone <andreipatea@gmail.com>
2026-09-09 21:29:20 +0100

    three pong

---
+++ samples/threepong/Game.cs
@@ -0,0 +1,10 @@
+using rasdaq;
+using rasdaq.Logging;
+
+public class Game : Application
+{
+    public override void Init()
+    {
+        Log.Info("Hello");
+    }
+}
---
+++ samples/threepong/Program.cs
@@ -0,0 +1,25 @@
+﻿using rasdaq;
+using rasdaq.Core.ECS;
+using rasdaq.Graphics;
+using rasdaq.Logging;
+using rasdaq.Resources;
+using rasdaq.Transformations;
+namespace threepong;
+
+class Program
+{
+    private static void Main()
+    {
+        try
+        {
+            using Game game = new();
+
+            game.Run(800, 600, "ThreePong");
+        }
+        catch (Exception ex)
+        {
+            File.WriteAllText("crash.log", ex.ToString());
+            throw new Exception(ex.Message + "\n Check 'rasdaq.log' for more details. \n" + ex.StackTrace);
+        }
+    }
+}
